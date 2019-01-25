05fd653279251c858e76811e71753dc76a44aa20
D4zZ <1686651776@qq.com>
2019-01-25 16:44:56 +0800

    注册页面JS（未完成）

--- javascript/register.js
+++ javascript/register.js
@@ -1,8 +1,10 @@
 ; function Register() {
     //属性
-    this.user = document.getElementsByTagName("input");
     this.kurumi = $(".Kurumi");
     this.tip = $(".tip");
+    this.kotori = $(".Kotori");
+    this.Tip = $(".Tip");
+    this.check = $(".check").children("input")[0];
     this.reg = {
         0: /^.{2,30}$/,
         1: /^\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}$/,
@@ -10,33 +12,52 @@
     }
     //初始化数据
     Register.prototype.initialization = function () {
-        this.kurumi = Array.from(this.kurumi);
-        this.tip = Array.from(this.tip);
+        
+        this.kurumi = $.makeArray(this.kurumi);
+        this.tip = $.makeArray(this.tip);
+        this.kotori = $.makeArray(this.kotori);
         this.bindingevents();
     }
     //事件绑定
     Register.prototype.bindingevents = function () {
         this.kurumi.forEach((item, index) => {
-            item.addEventListener("input", this.validation.bind(this, index));
+            $(item).on("input", this.validation.bind(this, index));
         })
-
+        this.kotori.forEach((item, index) => {
+            $(item).on("input", this.confirm.bind(this, index));
+        })
+        $("button").on("click",this.checkvalidation.bind(this));
     }
     //文本框验证
     Register.prototype.validation = function (index) {
-        this.sign = index;
-        if (this.reg[this.sign].test(this.kurumi[this.sign].value)) {
-            this.tip[this.sign].className = this.tip[this.sign].className.replace(/\sactive/g, "");
+        if (this.reg[index].test(this.kurumi[index].value)) {
+            $(this.tip[index]).removeClass(" active");
+            this.sheng = true;
         } else {
-            this.tip[this.sign].className = " active";
+            $(this.tip[index]).addClass(" active");
+            this.sheng = false;
+        }
+    }
+    //再次确认文本框验证{
+    Register.prototype.confirm = function (index) {
+        if (this.kotori[index].value === this.kurumi[index+1].value) {
+            $(this.Tip[index]).removeClass(" active");
+            this.Sheng = true;
+        } else {
+            $(this.Tip[index]).addClass(" active");
+            this.Sheng = false;
         }
     }
     //复选框获取返回值
     Register.prototype.checkvalidation = function () {
-
+        this.checkbool = this.check.checked;
+        if(this.sheng === true && this.Sheng === true && this.checkbool === true){
+            console.log(1)
+        }
     }
     //终极验证
     Register.prototype.clickverification = function () {
-
+        
     }
 }
 var register = new Register();
